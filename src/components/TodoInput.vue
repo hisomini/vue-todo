81bb543658427496f744d37a2ae4a0888a63f3e6
박소민 <sm.park@b2groups.com>
2022-07-31 19:52:14 +0900

    first commit

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+    <span class="addContainer" v-on:click="addTodo">
+      <i class="fa-solid fa-plus addBtn"></i>
+    </span>
+    <Modal v-if="showModal" @click="showModal = false">
+      <template v-slot:header>
+        <h3>경고
+          <i class="closeModalBtn fa-solid fa-xmark" @click="showModal = false"></i>
+        </h3>
+      </template>
+      <template v-slot:body>값을 입력하세요.</template>
+    </Modal>
+  </div>
+</template>
+
+<script>
+import Modal from './common/AlertModal.vue'
+
+export default {
+  components:{
+    Modal
+
+  },
+  data() {
+    return {
+      newTodoItem: "",
+      showModal: false
+    }
+  },
+  methods: {
+    addTodo(){
+      if(this.newTodoItem !== '') {
+        this.$emit('addTodoItem', this.newTodoItem);
+        this.clearInput();
+      } else {
+        this.showModal = !this.showModal;
+      }
+    },
+    clearInput() {
+      this.newTodoItem = '';
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478FB, #8763FB);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
+.closeModalBtn {
+  color: #42b983;
+}
+</style>
